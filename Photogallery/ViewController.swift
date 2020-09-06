66ee15e62204b3156b5b2a49ae06565ee5aeb2e5
olroma23 <roliynyk68@gmail.com>
2020-09-06 12:34:19 +0300

    Initial Commit

---
+++ Photogallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photogallery
+//
+//  Created by Roman Oliinyk on 06.09.2020.
+//  Copyright © 2020 Roman Oliinyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
